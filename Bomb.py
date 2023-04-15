ade3e83684390785e93b332233a7f41ab33cf234
PDykes1996 <pdykes1996@gmail.com>
2023-04-14 23:31:49 -0500

    Bomb Class
    Crearted Bomb Class

---
+++ Bomb.py
@@ -0,0 +1,11 @@
+import random
+class Bomb:
+    def Bomb(self, xCoord, yCoord, active):
+        icon = "|!!|"
+        xCoord = self.xCoord
+        yCoord = self.yCoord
+        active = self.active
+
+
+
+
